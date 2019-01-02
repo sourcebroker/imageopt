7b966cd7822234f092ce073888a03313a3725b79
l00k <l00k@protonmail.com>
2019-01-02 04:20:14 +0100

    Implementation of ImageOptim executor based on old code.

--- Classes/Executor/OptimizationExecutorRemoteImageoptim.php
+++ Classes/Executor/OptimizationExecutorRemoteImageoptim.php
@@ -30,21 +30,105 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class OptimizationExecutorRemoteImageoptim extends OptimizationExecutorRemote
 {
+
     /**
-     * Optimize image using remote Imageoptim
-     * Return the temporary file path
+     * Optimize image
      *
-     * @param string $inputImageAbsolutePath Absolute path/file with image to be optimized. It will be replaced with optimized version.
-     * @param Configurator $configurator Executor configurator
-     * @return ExecutorResult Executor Result
+     * @param $inputImageAbsolutePath string Absolute path/file with image to be optimized
+     * @param Configurator $configurator
+     * @return ExecutorResult Optimization result
      */
     public function optimize(string $inputImageAbsolutePath, Configurator $configurator) : ExecutorResult
     {
         $executorResult = GeneralUtility::makeInstance(ExecutorResult::class);
         $executorResult->setExecutedSuccessfully(false);
 
-        // Implement optimize image with Imageoptim and fill all $execuroeResult fields
+        if (!empty($configurator->getOption('api.key'))) {
+            $executorResult->setSizeBefore(filesize($inputImageAbsolutePath));
+            $this->initialize([
+                'auth' => [
+                    'api_key' => $configurator->getOption('api.key'),
+                ],
+                'url'  => [
+                    'upload' => 'https://im2.io',
+                ],
+            ]);
+            $this->upload($inputImageAbsolutePath, $configurator->getOption('options'));
+            $executorResult->setSizeAfter(filesize($inputImageAbsolutePath));
+            $executorResult->setExecutedSuccessfully(true);
+        }
 
         return $executorResult;
+    }
+
+    /**
+     * Upload file to imageoptim.com and save it if optimization will be success
+     *
+     * @param string $inputImageAbsolutePath Absolute path/file with original image
+     * @param array $options Additional options to optimize
+     * @return array Result of optimization
+     */
+    public function upload($inputImageAbsolutePath, $options = [])
+    {
+        $file = curl_file_create($inputImageAbsolutePath);
+
+        $optionsString = '';
+        foreach ($options as $name => $value) {
+            $optionsString .= ($optionsString ? ',' : '');
+            if (is_numeric($name)) {
+                $optionsString .= $value;
+            } else {
+                $optionsString .= $name . '=' . $value;
+            }
+        }
+
+        $url[] = $this->settings['url']['upload'];
+        $url[] = $this->settings['auth']['api_key'];
+        $url[] = $optionsString;
+        $fullUrl = implode('/', $url);
+
+        $result = self::request([
+            'file' => $file,
+        ], $fullUrl);
+
+        if ($result['success']) {
+            if (isset($result['response'])) {
+                file_put_contents($inputImageAbsolutePath, $result['response']);
+            } else {
+                $result['success'] = false;
+            }
+            unset($result['response']);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Executes request to remote server
+     *
+     * @param array $data Array with data of file
+     * @param string $url Url to execute request
+     * @param array $params Additional parameters
+     * @return array
+     */
+    public function request($data, $url, $params = [])
+    {
+        $options = [
+            'curl' => [],
+        ];
+
+        $responseFromAPI = parent::request($data, $url, $options);
+
+        if ($responseFromAPI['http_code'] != 200) {
+            return [
+                'success'       => false,
+                'providerError' => 'Url HTTP code: ' . $responseFromAPI['http_code'],
+            ];
+        }
+
+        return [
+            'success'  => true,
+            'response' => $responseFromAPI['response'],
+        ];
     }
 }
