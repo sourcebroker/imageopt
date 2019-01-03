bd8dc27e03a1b010b5e1c567457e0a375f4858ef
Lukasz Dobrowolski <l00k@protonmail.com>
2019-01-03 04:39:32 +0100

    Refactoring, error handling

--- Classes/Executor/OptimizationExecutorRemoteTinypng.php
+++ Classes/Executor/OptimizationExecutorRemoteTinypng.php
@@ -33,7 +33,6 @@ class OptimizationExecutorRemoteTinypng extends OptimizationExecutorRemote
 
     /**
      * Optimize image using remote Tinypng
-     * Return the temporary file path
      *
      * @param string $inputImageAbsolutePath Absolute path/file with image to be optimized. It will be replaced with optimized version.
      * @param Configurator $configurator Executor configurator
@@ -51,62 +50,49 @@ class OptimizationExecutorRemoteTinypng extends OptimizationExecutorRemote
                     'api_key' => $configurator->getOption('api.key'),
                 ],
                 'url'  => [
-                    'upload' => 'https://api.tinify.com/shrink',
+                    'upload' => $configurator->getOption('api.url.upload'),
                 ],
             ]);
 
-            $this->upload($inputImageAbsolutePath, $configurator->getOption('options'));
-            $executorResult->setSizeAfter(filesize($inputImageAbsolutePath));
-            $executorResult->setExecutedSuccessfully(true);
+            $result = $this->upload($inputImageAbsolutePath, $configurator->getOption('options'));
+
+            if ($result['success']) {
+                $download = $this->getFileFromRemoteServer($inputImageAbsolutePath,
+                    $result['response']['output']['url']);
+
+                if ($download) {
+                    $executorResult->setSizeAfter(filesize($inputImageAbsolutePath));
+                    $executorResult->setExecutedSuccessfully(true);
+                } else {
+                    $executorResult->setErrorMessage('Unable to download image');
+                }
+            } else {
+                $message = isset($result['providerError'])
+                    ? $result['providerError']
+                    : 'Undefined error';
+                $executorResult->setErrorMessage($message);
+            }
+        } else {
+            $executorResult->setErrorMessage('Set API account data in config file');
         }
 
         return $executorResult;
     }
 
     /**
-     * Upload file to tinypng.com and save it if optimization will be success
+     * Upload file to tinypng.com
      *
      * @param string $inputImageAbsolutePath Absolute path/file with original image
      * @param array $options Additional options to optimize
      * @return array Result of optimization
      */
-    public function upload($inputImageAbsolutePath, $options = [])
+    protected function upload(string $inputImageAbsolutePath, $options = [])
     {
         $result = self::request(file_get_contents($inputImageAbsolutePath), $this->settings['url']['upload']);
 
         if ($result['success']) {
-            if (isset($result['response']['output']['url'])) {
-                if (!$this->getFileFromRemoteServer($inputImageAbsolutePath, $result['response']['output']['url'])) {
-                    $result['success'] = false;
-                }
-            } else {
+            if (!isset($result['response']['output']['url'])) {
                 $result['success'] = false;
-            }
-            unset($result['response']);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Function parsing headers from response
-     *
-     * @param string $headers Headers from response
-     * @return array Array created from headers
-     */
-    protected static function parseHeaders($headers)
-    {
-        if (!is_array($headers)) {
-            $headers = explode("\r\n", $headers);
-        }
-        $result = [];
-        foreach ($headers as $header) {
-            if (empty($header)) {
-                continue;
-            }
-            $split = explode(':', $header, 2);
-            if (count($split) === 2) {
-                $result[strtolower($split[0])] = trim($split[1]);
             }
         }
 
@@ -121,7 +107,7 @@ class OptimizationExecutorRemoteTinypng extends OptimizationExecutorRemote
      * @param array $params Additional parameters
      * @return array Result of optimization includes the response from the tinypng.com
      */
-    public function request($data, $url, $params = [])
+    protected function request($data, $url, $params = [])
     {
         $options = array_merge([
             'curl' => [
@@ -132,27 +118,30 @@ class OptimizationExecutorRemoteTinypng extends OptimizationExecutorRemote
 
         $responseFromAPI = parent::request($data, $url, $options);
 
-        if (is_string($responseFromAPI['response'])) {
+        if ($responseFromAPI['error']) {
+            $result = [
+                'success'       => false,
+                'providerError' => 'cURL Error: ' . $responseFromAPI['error'],
+            ];
+        } elseif ($responseFromAPI['http_code'] === 429) {
+            $this->deactivateService();
+
+            $email = $this->getConfiguration()->getOption('limits.notification.reciver.email');
+            $this->sendNotificationEmail($email, 'Your limit has been exceeded',
+                'Your limit for Tinypng.com has been exceeded');
+
+            $result = [
+                'success'       => false,
+                'providerError' => 'Limit out',
+            ];
+        } elseif ($responseFromAPI['http_code'] !== 201) {
+            $result = [
+                'success'       => false,
+                'providerError' => 'Url HTTP code: ' . $responseFromAPI['http_code'],
+            ];
+        } elseif (is_string($responseFromAPI['response'])) {
             $headers = self::parseHeaders(substr($responseFromAPI['response'], 0, $responseFromAPI['header_size']));
             $body = substr($responseFromAPI['response'], $responseFromAPI['header_size']);
-
-            if ($responseFromAPI['http_code'] == 429) {
-                $this->deactivateService();
-
-                $email = $this->getConfiguration()->getOption('limits.notification.reciver.email');
-                $this->sendNotificationEmail($email, 'Your limit has been exceeded',
-                    'Your limit for Tinypng.com has been exceeded');
-
-                return [
-                    'success'       => false,
-                    'providerError' => 'Limit out',
-                ];
-            } elseif ($responseFromAPI['http_code'] != 201) {
-                return [
-                    'success'       => false,
-                    'providerError' => 'Url HTTP code: ' . $responseFromAPI['http_code'],
-                ];
-            }
 
             $result = [
                 'success'                   => true,
@@ -164,6 +153,31 @@ class OptimizationExecutorRemoteTinypng extends OptimizationExecutorRemote
                 'success'       => false,
                 'providerError' => 'cURL Error: ' . $responseFromAPI['error'],
             ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Function parsing headers from response
+     *
+     * @param string $headers Headers from response
+     * @return array Array created from headers
+     */
+    protected static function parseHeaders(string $headers)
+    {
+        if (!is_array($headers)) {
+            $headers = explode("\r\n", $headers);
+        }
+        $result = [];
+        foreach ($headers as $header) {
+            if (empty($header)) {
+                continue;
+            }
+            $split = explode(':', $header, 2);
+            if (count($split) === 2) {
+                $result[strtolower($split[0])] = trim($split[1]);
+            }
         }
 
         return $result;
