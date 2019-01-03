d47e6980ffb748f43137976a14bc7b792006b67f
Lukasz Dobrowolski <l00k@protonmail.com>
2019-01-03 07:52:09 +0100

    Implementing ImageOptim remote exectutor

--- Classes/Executor/OptimizationExecutorRemoteImageoptim.php
+++ Classes/Executor/OptimizationExecutorRemoteImageoptim.php
@@ -25,26 +25,119 @@
 namespace SourceBroker\Imageopt\Executor;
 
 use SourceBroker\Imageopt\Configuration\Configurator;
-use SourceBroker\Imageopt\Domain\Model\ExecutorResult;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class OptimizationExecutorRemoteImageoptim extends OptimizationExecutorRemote
 {
+
     /**
-     * Optimize image using remote Imageoptim
-     * Return the temporary file path
+     * Initialize executor
      *
-     * @param string $inputImageAbsolutePath Absolute path/file with image to be optimized. It will be replaced with optimized version.
-     * @param Configurator $configurator Executor configurator
-     * @return ExecutorResult Executor Result
+     * @param Configurator $configurator
+     * @return bool
      */
-    public function optimize(string $inputImageAbsolutePath, Configurator $configurator) : ExecutorResult
+    protected function initialize(Configurator $configurator) : bool
     {
-        $executorResult = GeneralUtility::makeInstance(ExecutorResult::class);
-        $executorResult->setExecutedSuccessfully(false);
+        $result = parent::initialize($configurator);
 
-        // Implement optimize image with Imageoptim and fill all $execuroeResult fields
+        if ($result) {
+            if (!isset($this->auth['key'])) {
+                $result = false;
+            } elseif (!isset($this->url['upload'])) {
+                $result = false;
+            }
+        }
 
-        return $executorResult;
+        return $result;
+    }
+
+    /**
+     * Upload file to imageoptim.com and save it if optimization will be success
+     *
+     * @param string $inputImageAbsolutePath Absolute path/file with original image
+     * @return array
+     */
+    protected function process(string $inputImageAbsolutePath) : array
+    {
+        $file = curl_file_create($inputImageAbsolutePath);
+
+        $optionsString = '';
+        foreach ($this->options as $name => $value) {
+            $optionsString .= ($optionsString ? ',' : '');
+            if (is_numeric($name)) {
+                $optionsString .= $value;
+            } else {
+                $optionsString .= $name . '=' . $value;
+            }
+        }
+
+        $url[] = $this->url['upload'];
+        $url[] = $this->auth['key'];
+        $url[] = $optionsString;
+        $fullUrl = implode('/', $url);
+
+        $result = self::request(['file' => $file], $fullUrl);
+
+        if ($result['success']) {
+            if (isset($result['response'])) {
+                $saved = $this->save($inputImageAbsolutePath, $result['response']);
+
+                if (!$saved) {
+                    $result['success'] = false;
+                    $result['providerError'] = 'Unable to save image';
+                }
+            } else {
+                $result['success'] = false;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Save image data into file
+     *
+     * @param string $outputImageAbsolutePath
+     * @param string $imageData
+     * @return bool
+     */
+    protected function save(string $outputImageAbsolutePath, string $imageData)
+    {
+        return (bool)file_put_contents($outputImageAbsolutePath, $imageData);
+    }
+
+    /**
+     * Executes request to remote server
+     *
+     * @param array $data Array with data of file
+     * @param string $url Url to execute request
+     * @param array $params Additional parameters
+     * @return array
+     */
+    protected function request($data, $url, $params = [])
+    {
+        $options = [
+            'curl' => [],
+        ];
+
+        $responseFromAPI = parent::request($data, $url, $options);
+
+        if ($responseFromAPI['error']) {
+            $result = [
+                'success'       => false,
+                'providerError' => 'cURL Error: ' . $responseFromAPI['error'],
+            ];
+        } elseif ($responseFromAPI['http_code'] !== 200) {
+            $result = [
+                'success'       => false,
+                'providerError' => 'Url HTTP code: ' . $responseFromAPI['http_code'],
+            ];
+        } else {
+            $result = [
+                'success'  => true,
+                'response' => $responseFromAPI['response'],
+            ];
+        }
+
+        return $result;
     }
 }
