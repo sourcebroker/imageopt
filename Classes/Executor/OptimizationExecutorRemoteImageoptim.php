86eca5aaeaed8d34f13952459379556669fa2847
Lukasz Dobrowolski <l00k@protonmail.com>
2019-01-03 05:39:51 +0100

    Implementing ImageOptim exectutor. Refactoring and error handling. CS fix

--- Classes/Executor/OptimizationExecutorRemoteImageoptim.php
+++ Classes/Executor/OptimizationExecutorRemoteImageoptim.php
@@ -30,21 +30,135 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class OptimizationExecutorRemoteImageoptim extends OptimizationExecutorRemote
 {
+
     /**
-     * Optimize image using remote Imageoptim
-     * Return the temporary file path
+     * Optimize image
      *
-     * @param string $inputImageAbsolutePath Absolute path/file with image to be optimized. It will be replaced with optimized version.
-     * @param Configurator $configurator Executor configurator
-     * @return ExecutorResult Executor Result
+     * @param string $inputImageAbsolutePath Absolute path/file with image to be optimized
+     * @param Configurator $configurator
+     * @return ExecutorResult Optimization result
      */
     public function optimize(string $inputImageAbsolutePath, Configurator $configurator) : ExecutorResult
     {
         $executorResult = GeneralUtility::makeInstance(ExecutorResult::class);
         $executorResult->setExecutedSuccessfully(false);
 
-        // Implement optimize image with Imageoptim and fill all $execuroeResult fields
+        if (!empty($configurator->getOption('api.key'))) {
+            $executorResult->setSizeBefore(filesize($inputImageAbsolutePath));
+            $this->initialize([
+                'auth' => [
+                    'api_key' => $configurator->getOption('api.key'),
+                ],
+                'url'  => [
+                    'upload' => $configurator->getOption('api.url.upload'),
+                ],
+            ]);
+            $result = $this->upload($inputImageAbsolutePath, $configurator->getOption('options'));
+
+            if ($result['success']) {
+                $saved = $this->save($inputImageAbsolutePath, $result['response']);
+
+                if ($saved) {
+                    $executorResult->setSizeAfter(filesize($inputImageAbsolutePath));
+                    $executorResult->setExecutedSuccessfully(true);
+                } else {
+                    $executorResult->setErrorMessage('Unable to save image');
+                }
+            } else {
+                $message = isset($result['providerError'])
+                    ? $result['providerError']
+                    : 'Undefined error';
+                $executorResult->setErrorMessage($message);
+            }
+        } else {
+            $executorResult->setErrorMessage('Set API account data in config file');
+        }
 
         return $executorResult;
+    }
+
+    /**
+     * Upload file to imageoptim.com
+     *
+     * @param string $inputImageAbsolutePath Absolute path/file with original image
+     * @param array $options Additional options to optimize
+     * @return array Result of optimization
+     */
+    protected function upload(string $inputImageAbsolutePath, $options = [])
+    {
+        $file = curl_file_create($inputImageAbsolutePath);
+
+        $optionsString = '';
+        foreach ($options as $name => $value) {
+            $optionsString .= ($optionsString ? ',' : '');
+            if (is_numeric($name)) {
+                $optionsString .= $value;
+            } else {
+                $optionsString .= $name . '=' . $value;
+            }
+        }
+
+        $url[] = $this->settings['url']['upload'];
+        $url[] = $this->settings['auth']['api_key'];
+        $url[] = $optionsString;
+        $fullUrl = implode('/', $url);
+
+        $result = self::request(['file' => $file], $fullUrl);
+
+        if ($result['success']) {
+            if (!isset($result['response'])) {
+                $result['success'] = false;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Save image data into file
+     *
+     * @param string $outputImageAbsolutePath
+     * @param string $imageData
+     * @return bool
+     */
+    protected function save(string $outputImageAbsolutePath, string $imageData)
+    {
+        return (bool)file_put_contents($outputImageAbsolutePath, $imageData);
+    }
+
+    /**
+     * Executes request to remote server
+     *
+     * @param array $data Array with data of file
+     * @param string $url Url to execute request
+     * @param array $params Additional parameters
+     * @return array
+     */
+    protected function request($data, $url, $params = [])
+    {
+        $options = [
+            'curl' => [],
+        ];
+
+        $responseFromAPI = parent::request($data, $url, $options);
+
+        if ($responseFromAPI['error']) {
+            $result = [
+                'success'       => false,
+                'providerError' => 'cURL Error: ' . $responseFromAPI['error'],
+            ];
+        } elseif ($responseFromAPI['http_code'] !== 200) {
+            $result = [
+                'success'       => false,
+                'providerError' => 'Url HTTP code: ' . $responseFromAPI['http_code'],
+            ];
+        } else {
+            $result = [
+                'success'  => true,
+                'response' => $responseFromAPI['response'],
+            ];
+        }
+
+        return $result;
     }
 }
