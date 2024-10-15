b8c7f9f269995d87ab912a5bcd5172a28b672bee
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2024-10-15 11:20:47 +0200

    Rm not needed files. Up with TYPO3 version in ext_emconf.php requs.

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '11.5.99',
+                    'typo3' => '12.4.99',
                 ],
             'conflicts' =>
                 [],
